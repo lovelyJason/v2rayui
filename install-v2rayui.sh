c7ad5646b8b671feb084c465812846450a5cc9ed
jasonhuang <1248394650@qq.com>
2021-09-25 12:29:26 +0800

    修改模板

--- install-v2rayui.sh
+++ install-v2rayui.sh
@@ -47,6 +47,18 @@ chmod +x /usr/local/bin/v2rayui
 chmod +x /usr/local/v2rayui/start.sh      # web service启动入口
 
 # 配置supervisorctl
+# [program:test] #程序的名字，在supervisor中可以用这个名字来管理该程序。
+# user=root #指定运行用户
+# command=bash /root/1.sh #启动程序的命令
+# autorstart=true #设置改程序是否虽supervisor的启动而启动
+# directory=/home/lege #相当于在该目录下执行程序
+# autorestart=true #程序停止之后是否需要重新将其启动
+# startsecs=5 #重新启动时，等待的时间
+# startretries=100 #重启程序的次数
+# redirect_stderr=true #是否将程序错误信息重定向的到文件
+# stdout_logfile=/home/lege/supervisor_log/log.txt #将程序输出重定向到该文件
+# stderr_logfile=/home/lege/supervisor_log/err.txt #将程序错误信息重定向到该文件
+
 mkdir /etc/supervisor
 mkdir /etc/supervisor/conf.d
 echo_supervisord_conf > /etc/supervisor/supervisord.conf
@@ -57,14 +69,23 @@ EOF
 touch /etc/supervisor/conf.d/v2rayui.ini
 cat>>/etc/supervisor/conf.d/v2rayui.ini<<EOF
 [program:v2rayui]
-command=/usr/local/v2rayui/start.sh run
-stdout_logfile=/var/log/v2rayui
+user=root
+command=bash /usr/local/v2rayui/start.sh
+stderr_logfile=/var/log/v2ray.err.log
+stdout_logfile=/var/log/v2rayui.out.log
 autostart=true
 autorestart=true
 startsecs=5
 priority=1
 stopasgroup=true
 killasgroup=true
+
+environment=LANG="en_US.UTF-8",LC_ALL="en_US.UTF-8",LC_LANG="en_US.UTF-8"
+
+[inet_http_server]
+port=0.0.0.0:9001
+username=user
+password=123
 EOF
 supervisorctl reload
 
